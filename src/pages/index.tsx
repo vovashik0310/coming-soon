9e5326f330dbcc9444d6dd67dfaa2cd04f44a79e
vovashik0310 <v9873826@gmail.com>
2024-08-21 04:10:57 +0300

    [feat] change the font of the coming soon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,11 +11,11 @@ const syne = Syne({ subsets: ["latin"], display: "swap" });
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center">
-      <h1 className={`text-5xl text-white font-bold mb-8 animate-pulse ${syne.className}`}>
+      <h1 className={`text-6xl text-white font-bold mb-8 animate-pulse ${syne.className}`}>
         Coming Soon
       </h1>
-      <p className={`text-white text-lg mb-8 ${roboto.className}`}>
-        Offset Prototypes is coming soon.
+      <p className={`text-2xl text-white text-lg mb-8 ${roboto.className}`}>
+        Offset Prototypes is coming soon
       </p>
     </div>
   );
